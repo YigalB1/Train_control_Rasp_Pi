974f7eb889808457bca9a0d0f38a906f22d6dd93
YigalB <yigalb@hotmail.com>
2020-07-18 23:35:22 +0300

    Add files via upload

--- TrainMotor.py
+++ TrainMotor.py
@@ -33,28 +33,36 @@ def do_this_on_close():
  
 def do_start_motor():
     my_train.motor_start()
+    button_message.text="Starting motor"
     
 def do_shut_down_motor():
+    my_train.speed = 0 
+    button_speed.text = my_train.speed
     my_train.motor_shut_down()
+    button_message.text="Shutting down motor"
 
 def do_left_direction():
     my_train.set_direction(LEFT)
+    button_message.text="Going left"
 
 def do_right_direction():
-    my_train.set_direction(RIGHT)
+    my_train.set_direction(RIGHT)    
+    button_message.text="Going right"
 
 def do_increase_speed():
     my_train.motor_increase_speed()
     #train_defs.menu_motor_increase_speed(my_train)
     button_speed.text = my_train.speed
+    button_message.text="Speeding up"
     
 def do_descrease_speed():
     my_train.motor_decrease_speed()
     #train_defs.menu_motor_decrease_speed(my_train)
     button_speed.text = my_train.speed
+    button_message.text="Slowing Down"
     
 def do_display_speed():
-    train_defs.menu_motor_decrease_speed(my_train)
+    menu_motor_decrease_speed(my_train)
     button_speed.text = my_train.speed
 
       
@@ -65,6 +73,7 @@ def do_slider_speed():
     my_train.motor_set_speed(slider_speed.value)
 
 def do_exit_menu():
+    button_message.text="Exiting"
     my_train.train_exit()
     app.destroy()
     sys.exit()
@@ -76,7 +85,25 @@ text_left = "Go Left"
 text_right = "Go Right"
 text_speed = "Speed: " + str(my_train.speed)
 
-app = App(title="Train Control menu", bg = "gray",width=500, height=600)
+
+app = App(title="Train Control menu", bg = "gray",width=500, height=700)
+
+
+
+# TBD does not work. How to send messages to the GUI?
+#text_msg.after(1000)
+# I am using a button as a display, because I do not know how to write to test box
+
+
+space_box0 = Box(app, width=500, height=50)
+space_box0.border = False
+#text_msg = Text(space_box0, text=my_train.msg_to_display)
+
+button_message = PushButton(space_box0, command=do_nothing,
+                          text="",width=50,height=5,align="left")
+button_message.text_color="blue"
+button_message.text_size=25
+
 
 power_box = Box(app, width=500, height=100)
 power_box.border = False
@@ -128,7 +155,7 @@ button_slower.bg="blue"
 button_slower.text_size=20
 
 text_speed="  0  "
-button_speed = PushButton(speed_box, command=do_display_speed, text=text_speed,height=4,align="right")
+button_speed = PushButton(speed_box, command=do_nothing, text=text_speed,height=4,align="right")
 button_speed.text_color="blue"
 button_speed.bg="white"
 button_speed.text_size=40
--- Train_classes.py
+++ Train_classes.py
@@ -11,7 +11,29 @@ in1 = 24 # blue wire
 in2 = 23 # brown wire 
 en = 25  # gray wire
 
-#GPIO.cleanup()
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(in1,GPIO.OUT)
+GPIO.setup(in2,GPIO.OUT)
+GPIO.setup(en,GPIO.OUT)
+speed_control = GPIO.PWM(en,1000)
+
+'''
+# check if GPIO channels are active
+# if yes: clean them
+# 0 means Output
+# 1 means Input
+
+func=GPIO.gpio_function(in1)
+print (func)
+print (GPIO.gpio_function(in1))
+print (GPIO.gpio_function(in2))
+print (GPIO.gpio_function(en))
+
+if (func==0):
+    GPIO.cleanup(in1)
+    GPIO.cleanup(in2)
+    GPIO.cleanup(en)
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(in1,GPIO.OUT)
 GPIO.setup(in2,GPIO.OUT)
@@ -19,7 +41,10 @@ GPIO.setup(en,GPIO.OUT)
 
 GPIO.output(in1,GPIO.LOW)
 GPIO.output(in2,GPIO.LOW)
-speed_control = GPIO.PWM(en,1000)
+'''
+
+
+
 
 class train:
     def __init__(self):
@@ -27,23 +52,37 @@ class train:
         self.max_speed=100
         self.speed_delta = 5
         self.speed = 0 # int between  to 100        
-        self.train_nginecar_keys = None  # TODO - do an enum of KEYS_ON and KEYS_OFF, to stop/start the car
+        self.train_enginecar_keys = None  # TODO - do an enum of KEYS_ON and KEYS_OFF, to stop/start the car
         self.direction = RIGHT # left or right
+        self.keys_on = False
+        self.msg_to_display=""
+        GPIO.output(in1,GPIO.LOW)
+        GPIO.output(in2,GPIO.LOW)
         
         
     def motor_start(self):
         #GPIO.clenup()
         self.speed=0
         speed_control.start(0)
+        self.msg_to_display="starting Engine"
+        self.keys_on=True
+        self.set_direction(self.direction) # keep the last direction
+        
 
-    def motor_shut_down(self):        
-        self.speed=0
-        speed_control.stop()
+    def motor_shut_down(self):
+        if (self.keys_on==True):
+            self.keys_on=False
+            self.speed=0
+            speed_control.stop()            
+            GPIO.output(in1,GPIO.LOW)
+            GPIO.output(in2,GPIO.LOW)
         
 
     def set_direction(self, _direction):
         # TBD - add check if direction is changing.
         # if yes: wait 2 seconds to avoid damage to engine
+        if (self.keys_on==False):
+            return
         if _direction==RIGHT:
             self.direction = RIGHT
             GPIO.output(in1,GPIO.HIGH)
@@ -55,24 +94,31 @@ class train:
             GPIO.output(in2,GPIO.HIGH)
             #print("<<<",end='')
 
-    def motor_increase_speed(self):        
+    def motor_increase_speed(self):
+        if (self.keys_on==False):
+            return
         self.speed += self.speed_delta
         if (self.speed > self.max_speed):
             self.speed = self.max_speed;
         speed_control.ChangeDutyCycle(self.speed)
 
     def motor_decrease_speed(self):
+        if (self.keys_on==False):
+            return
         self.speed -= self.speed_delta
         if (self.speed < self.min_speed):
             self.speed = self.min_speed
         speed_control.ChangeDutyCycle(self.speed)
     
     def train_exit(self):
+        self.keys_on=False
         speed_control.stop()
         GPIO.cleanup()
     
     # assuming direction was set already
     def motor_set_speed(self,_speed):
+        if (self.keys_on==False):
+            return
         self.speed = _speed
         speed_control.ChangeDutyCycle(self.speed)
         
