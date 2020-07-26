a6583ea3308383720af7de446bed56a3a3e117cf
adi-bzn <adi.benzeev@gmail.com>
2020-07-26 20:09:43 +0300

    Wrap all the GPIO usages in an if statement to check if the os is posix, linux.

--- GPIO_handler.py
+++ GPIO_handler.py
@@ -1,4 +1,7 @@
-import RPi.GPIO as GPIO
+import os
+
+if os.name == "posix":
+    import RPi.GPIO as GPIO
 
 
 # HW pins  connecting to the L298N motor driver,
@@ -9,11 +12,12 @@ in1 = 24  # blue wire
 in2 = 23  # brown wire
 en = 25  # gray wire
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(in1, GPIO.OUT)
-GPIO.setup(in2, GPIO.OUT)
-GPIO.setup(en, GPIO.OUT)
-speed_control = GPIO.PWM(en, 1000)
+if os.name == "posix":
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(in1, GPIO.OUT)
+    GPIO.setup(in2, GPIO.OUT)
+    GPIO.setup(en, GPIO.OUT)
+    speed_control = GPIO.PWM(en, 1000)
 
 '''
 # check if GPIO channels are active
@@ -43,28 +47,34 @@ GPIO.output(in2,GPIO.LOW)
 
 
 def initialize():
-    GPIO.output(in1, GPIO.LOW)
-    GPIO.output(in2, GPIO.LOW)
+    if os.name == "posix":
+        GPIO.output(in1, GPIO.LOW)
+        GPIO.output(in2, GPIO.LOW)
 
 
 def shut_down():
-    GPIO.output(in1, GPIO.LOW)
-    GPIO.output(in2, GPIO.LOW)
+    if os.name == "posix":
+        GPIO.output(in1, GPIO.LOW)
+        GPIO.output(in2, GPIO.LOW)
 
 
 def cleanup():
-    GPIO.cleanup()
+    if os.name == "posix":
+        GPIO.cleanup()
 
 
 def get_speed_control():
-    return speed_control
+    if os.name == "posix":
+        return speed_control
 
 
 def set_right():
-    GPIO.output(in1, GPIO.HIGH)
-    GPIO.output(in2, GPIO.LOW)
+    if os.name == "posix":
+        GPIO.output(in1, GPIO.HIGH)
+        GPIO.output(in2, GPIO.LOW)
 
 
 def set_left():
-    GPIO.output(in1, GPIO.LOW)
-    GPIO.output(in2, GPIO.HIGH)
+    if os.name == "posix":
+        GPIO.output(in1, GPIO.LOW)
+        GPIO.output(in2, GPIO.HIGH)
